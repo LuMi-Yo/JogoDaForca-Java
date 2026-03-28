dfa0571092de1e386c76d7f9fef98a9e4b5d35b3
LuMi-Yo <luismiguelcarvalho65@gmail.com>
2026-03-28 17:07:01 -0300

    Implementa método JogoDaForca e atualiza conteúdo do palavras.txt

--- JogoDaForca/src/JogoDaForca.java
+++ JogoDaForca/src/JogoDaForca.java
@@ -1,4 +1,23 @@
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 class JogoDaForca {
-
+	private ArrayList<String> listadepalavras;
+	
+	public JogoDaForca() {
+		InputStream stream = this.getClass().getResourceAsStream("/dados/palavras.txt");
+		if (stream == null) {
+		JOptionPane.showMessageDialog(null, "Arquivo de palavras inexistente!");
+			System.exit(0);
+		}
+		Scanner arquivo = new Scanner(stream);
+		String linha;
+		while (arquivo.hasNext()) {
+			linha = arquivo.nextLine();
+			this.listadepalavras.add(linha);
+		}
+		arquivo.close();
+	}
 }
