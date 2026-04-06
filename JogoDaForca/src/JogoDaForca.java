e6ccb05f51f602f45da8c1d6cd56296bae8196af
LuMi-Yo <luismiguelcarvalho65@gmail.com>
2026-04-06 15:36:00 -0300

    Implementa front-end completo.

--- JogoDaForca/src/JogoDaForca.java
+++ JogoDaForca/src/JogoDaForca.java
@@ -1,3 +1,4 @@
+import java.awt.HeadlessException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,44 +27,52 @@ class JogoDaForca {
 	
 	//Métodos -> tirando aquele que foi o gemini que fez, o resto eu fiz logo tudo, mas tu pode apagar e refazer os teus 
 	
-	public JogoDaForca() {
-		InputStream stream = this.getClass().getResourceAsStream("/dados/palavras.txt");
-		if (stream == null) {
-		JOptionPane.showMessageDialog(null, "Arquivo de palavras inexistente!");
-			System.exit(0);
+	public void jogoDaForca() {
+		try {
+			InputStream stream = this.getClass().getResourceAsStream("/dados/palavras.txt");
+			if (stream == null) {
+			JOptionPane.showMessageDialog(null, "Arquivo de palavras inexistente!");
+				System.exit(0);
+			}
+			Scanner arquivo = new Scanner(stream);
+			String linha;
+			while (arquivo.hasNext()) {
+				linha = arquivo.nextLine();
+				this.listadepalavras.add(linha);
+			}
+			arquivo.close();
+		} catch (HeadlessException e) {
+			e.printStackTrace();
 		}
-		Scanner arquivo = new Scanner(stream);
-		String linha;
-		while (arquivo.hasNext()) {
-			linha = arquivo.nextLine();
-			this.listadepalavras.add(linha);
-		}
-		arquivo.close();
 	}
 	
 	public void iniciar() {
-		if(this.listadepalavras.isEmpty()) {
-			JOptionPane.showConfirmDialog(null, "Arquivo de palavras inexistente!");
-			System.exit(0);
+		try {
+			if(this.listadepalavras.isEmpty()) {
+				JOptionPane.showConfirmDialog(null, "Arquivo de palavras inexistente!");
+				System.exit(0);
+			}
+			
+			if (!this.palavraSorteada.isEmpty()) {
+				this.historicoResultados.add(this.palavraSorteada + " - " + this.getResultado() + System.lineSeparator());
+			}
+			
+			
+			Random gerador = new Random();
+			int indice = gerador.nextInt(this.listadepalavras.size());
+			
+			String[] partes = this.listadepalavras.get(indice).split(";");
+			this.palavraSorteada = partes[0].toUpperCase();
+			this.dicaPalavra = partes[1].toUpperCase();
+				
+			this.palavraOculta = new char[this.palavraSorteada.length()];
+			Arrays.fill(palavraOculta, '*');
+			this.acertos = 0;
+			this.codigoPenalidade = 0;
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		if (!this.palavraSorteada.isEmpty()) {
-			this.historicoResultados.add(this.palavraSorteada + '-' + this.getResultado());
-		}
-		
-		
-		Random gerador = new Random();
-		int indice = gerador.nextInt(this.listadepalavras.size());
-		
-		String[] partes = this.listadepalavras.get(indice).split(",");
-		this.palavraSorteada = partes[0].toUpperCase();
-		this.dicaPalavra = partes[1].toUpperCase();
-		
-		this.palavraOculta = new char[this.palavraSorteada.length()];
-		Arrays.fill(palavraOculta, '*');
-		this.acertos = 0;
-		this.codigoPenalidade = 0;
-		}
+		}	
 	
 	public String getDica() {
 		return this.dicaPalavra;
@@ -80,7 +89,7 @@ class JogoDaForca {
 	
 	//essa aqui foi totalmente gemini que fez só pra eu testar se tava rodando o código
 	
-	/*public ArrayList<Integer> getOcorrencias(String letra) throws Exception {
+	public ArrayList<Integer> getOcorrencias(String letra) throws Exception {
 		// Lança exceção se a string for vazia, nula ou tiver mais de 1 caractere
 		if (letra == null || letra.length() != 1) {
 			throw new Exception("A letra deve conter exatamente 1 caractere.");
@@ -112,7 +121,7 @@ class JogoDaForca {
 		}
 		
 		return posicoes;
-	}*/
+	}
 	
 	public boolean terminou(){
 		return !this.getResultado().equals("Em Andamento");
